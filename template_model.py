9924c6d8a88a6b5b7a8008ec35b801a735d94f01
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2025-03-23 17:04:48 +0800

    update common library

--- template_model.py
+++ template_model.py
@@ -1,11 +1,11 @@
-from common.ModelAPI import ModelHandler, Predict, ModelInfo
+from common.ModelAPI import ModelHandler, ModelType, Predict, ModelInfo
 
 class TemplateModel(ModelHandler):
     def invoke(self, request: Predict.Request):
         return Predict.Response([])
 
     def model_info(self) -> ModelInfo.Response:
-        return ModelInfo.Response("Template", "1.0")
+        return ModelInfo.Response("Template", "xiaoxigua", "1.0", ModelType.Predict)
 
 
 def setup() -> ModelHandler:
